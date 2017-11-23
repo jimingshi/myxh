f988873bb070cfdf84020ba975d25ab4134282f7
jimingshi <jms1983@126.com>
2017-11-23 09:48:19 +0800

    删除hx_security

--- hx_security/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import inherit_stock_picking
-import inherit_account_product_template
-import inherit_product_template
-import inherit_account_invoice
-import inherit_account_move
